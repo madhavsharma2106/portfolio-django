360deb2ed81b037dde9b90d6f85d11d49519bd84
Madhav <madhav.sharma@axisrooms.com>
2018-04-03 14:31:48 +0530

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+#Create a blog model
+#  Title
+#  Pub date
+#  body
+#  image
+
+
+#Add a blog app to root app settings
+
+#create a migration
+
+#migrate
+
+#add to the app admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
