a3bcdd0bea51a7d18c1910c290b2c6f6eee0b887
Madhav <madhav.sharma@axisrooms.com>
2018-04-03 16:54:17 +0530

    Blog functionality added && Now serchable by blog Id

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-def home(request):
-    return render(request, 'blog/home.html')
+def allblogs(request):
+    blog = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogs': blog})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog,pk=blog_id)
+    print(detail_blog)
+    return render(request, 'blog/detail.html', {'blog': detail_blog})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,12 @@
+from django.conf.urls import include    
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
-import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    path('blog/', blog.views.home, name='blog'),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
